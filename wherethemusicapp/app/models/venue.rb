cfddc8e89493b26fe7ecb2eabfb54e660edc707e
nuala33 <nuala33@gmail.com>
2015-05-18 21:05:17 -0400

    added addresses to the windows on the map, added devise

--- wherethemusicapp/app/models/venue.rb
+++ wherethemusicapp/app/models/venue.rb
@@ -16,15 +16,36 @@ class Venue
 			venue['venue']['lat'].nil? || venue['venue']['lng'].nil? || venue['venue']['displayName'] == "Unknown venue"
 		end
 
-		venues_with_coordinates.map do |venue|
-			{
-				name: venue['venue']['displayName'],
-				#date: venue['end']['date'],
-				artist: venue['displayName'],
-				link: venue['uri'],
-		    latitude: venue['venue']['lat'],
-		    longitude: venue['venue']['lng']
-		  }
+
+		Rails.cache.fetch("#{latitude}, #{longitude}", expires_in: 600) do
+			venues_with_coordinates.map do |venue|
+				{
+					name: venue['venue']['displayName'],
+					#date: venue['end']['date'],
+					artist: venue['displayName'],
+					link: venue['uri'],
+			    latitude: venue['venue']['lat'],
+			    longitude: venue['venue']['lng'],
+			    street: get_address(venue['venue']['id'])
+			    #website: get_address(venue['venue']['website'])
+			  }
+			end
 		end
 	end
+
+	def self.get_address(venue_id)
+		address_url = "http://api.songkick.com/api/3.0/venues/#{venue_id}.json?apikey=RU3SDbXa8aLvAcbX"
+		address_response = RestClient.get(address_url)
+		address_parsed_response = JSON.parse(address_response)
+		address_parsed_response['resultsPage']['results']['venue']['street']
+	end
+
+	#def self.get_website(venue_website)
+#		address_url = "http://api.songkick.com/api/3.0/venues/#{venue_id}.json?apikey=RU3SDbXa8aLvAcbX"
+#		address_response = RestClient.get(address_url)
+#		address_parsed_response = JSON.parse(address_response)
+#		address_parsed_response['resultsPage']['results']['venue']['website']
+#	end
+
+
 end
